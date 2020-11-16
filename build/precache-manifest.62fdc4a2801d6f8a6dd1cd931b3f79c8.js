c9ba792d54951b4448d3e613effd7e76d3b80dbb
Caio <caio.rodrigues.barbosa@usp.br>
2020-11-16 08:57:20 -0300

    deploy

---
+++ build/precache-manifest.62fdc4a2801d6f8a6dd1cd931b3f79c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94616c45fc51cf257feadadaa1093635",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d42d19f97978dc06ec5",
+    "url": "/static/js/2.472cbca3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.472cbca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4a8a53304470f3415f7",
+    "url": "/static/js/main.22fad681.chunk.js"
+  },
+  {
+    "revision": "82ca9cfee7653ddcbdc2",
+    "url": "/static/js/runtime-main.6db7de9c.js"
+  }
+]);
